55c3a7bf35c6e9fc28180bedd2f64c6a7460a560
Isaiah Walker <Walkisai.72@gmail.com>
2021-12-05 23:04:34 -0600

    Day02 solution (#2)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const day01 = require("./src/solutions/day01.js");
+const day02 = require("./src/solutions/day02.js");
 
 const init = () => {
   const requested_day = process.argv[2];
@@ -12,6 +13,17 @@ const init = () => {
       day01.getThreeMeasurementsSlidingWindowDepthIncreases()
     );
     return;
+  }
+
+  if (requested_day === "day02") {
+    console.log(
+      "Solution to day02 [PART 1] is: ",
+      day02.getHorizontalPostitionByFinalDepth()
+    );
+    console.log(
+      "Solution to day02 [PART 2] is: ",
+      day02.getHorizontalPostitionByFinalDepthWithAim()
+    );
   }
   console.error(`No solution found for given day: ${requested_day}.`);
 };
